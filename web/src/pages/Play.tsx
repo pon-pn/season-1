22dbe056c2cf8ca0e706231d289709324f5ccd76
mihoogura <miho@ryu.games>
2022-09-08 22:04:23 +0900

    Start making play page

--- web/src/pages/Play.tsx
+++ web/src/pages/Play.tsx
@@ -1,8 +1,22 @@
+import { Navbar } from "components/Navbar";
+
 const Play = () => {
   return (
-    <div>
-      <h1>this is play</h1>
-    </div>
+    <>
+      <Navbar />
+      <div className='flex flex-col items-center justify-center'>
+        <div className='mt-20'>
+          <p className='text-center text-xl'>現在の最後の単語は...</p>
+          {/* TODO: ここはコントラクトから最後の単語を取ってきて入れる */}
+          <p className='font-nico text-[128px]'>りんご</p>
+        </div>
+        <img src='public/images/arrow.png' alt='arrow' />
+        <div>
+          {/* TODO: 「」の中は動的に変える */}
+          <input placeholder='「ご」につづく単語を入れよう'></input>
+        </div>
+      </div>
+    </>
   );
 };
 
