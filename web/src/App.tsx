b5d95f9c87bc30c6123edce43ef1cbf1377383fc
Eiko0705 <eiko.imai.070502@gmail.com>
2022-09-10 19:23:12 +0900

    feat: create navbar

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
 import { lazy, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Navbar } from "components/Navbar";
 import { ROUTES } from "constants/routes";
 import { LoadingSpinner } from "components/Spinner";
 
@@ -13,6 +14,7 @@ const Play = lazy(() => import("pages/Play"));
 const App = () => (
   <BrowserRouter>
     <Suspense fallback={<LoadingSpinner />}>
+      <Navbar />
       <Routes>
         <Route path={ROUTES.TOP} element={<Home />} />
         <Route path={ROUTES.HOWTOPLAY} element={<HowToPlay />} />
--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -1,7 +1,28 @@
 export const Navbar = () => {
   return (
     <div>
-      <h1>this is home</h1>
+      <nav className="px-2 py-2.5 sm:px-4">
+        <div className="container justify-between items-center px-2 mx-auto flex flex-nowrap">
+          <div className="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
+            <a className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white" href="#pablo">
+              Creators Studio
+            </a>
+          </div>
+          <div className="">
+            <ul className="flex flex-col lg:flex-row list-none ml-auto">
+              <li className="nav-item">
+                <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">説明</a>
+              </li>
+              <li>
+                <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">参加する</a>
+              </li>
+            </ul>
+          </div>
+          <div className="">
+            <button>ウォレット接続</button>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 };
