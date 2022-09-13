de0e190ff01f39b3e46808f3f24f8f54ec2964b0
Eiko0705 <eiko.imai.070502@gmail.com>
2022-09-14 00:49:48 +0900

    fix: adjust the position

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -2,26 +2,28 @@ export const Navbar = () => {
   return (
     <div>
       <nav className="px-2 py-8 sm:px-4">
-        <div className="container justify-between items-center px-2 mx-auto flex flex-wrap">
-          <a className="flex items-center" href="">
+        <div className="justify-between items-center px-2 flex flex-nowrap">
+          <a className="flex w-56 items-center" href="">
             <img 
               src="public/images/creatorsstudio.jpeg"
               alt="Creators Studio Logo"
-              className="mr-3 sm:h-11 object-contain h-full rounded-lg"
+              className="ml-10 mr-5 sm:h-11 object-contain h-full rounded-lg"
             />
             <span className="self-center text-xl whitespace-nowrap text-white" >Creators Studio</span>
           </a>
-          <div className="px-4">
-            <ul className="flex flex-col lg:flex-row list-none ml-auto">
+          <div>
+            <ul className="flex lg:flex-row list-none ml-auto">
               <li>
-                <a className="px-10 py-2 flex items-center text-white hover:opacity-75">説明</a>
+                <a className="flex w-40 px-9 py-2 items-center text-white hover:opacity-75">説明</a>
               </li>
               <li>
-                <a className="px-10 py-2 flex items-center text-white hover:opacity-75">参加する</a>
+                <a className="flex w-40 px-9 py-2 items-center text-white hover:opacity-75">参加する</a>
               </li>
             </ul>
           </div>
-          <button className="bg-transparent text-white py-2 px-4 border border-pink-400 rounded-full" >ウォレット接続</button>
+          <div className="w-56">
+            <button className="bg-transparent text-white py-2 px-4 border border-pink-400 rounded-full" >ウォレット接続</button>
+          </div>
         </div>
       </nav>
     </div>
